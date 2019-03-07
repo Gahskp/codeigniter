6cbfe0dd772c3d42daf52fd222e5a534bbbf33b2
Gabriel Augusto de Oliveira <gahskp@gmail.com>
2019-03-07 11:32:36 -0300

    first steps

---
+++ application/views/produtos/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Produtos</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1>Produtos</h1>
+            <table class="table">
+                <?php foreach ($produtos as $produto): ?>
+                    <tr>
+                        <td><?= $produto["nome"] ?></td>
+                        <td><?= $produto["preco"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
+    </body>
+</html>
