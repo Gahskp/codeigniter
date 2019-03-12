0d3800e19f77b9ec1c1a900f057ffe2b88312513
Gabriel Augusto de Oliveira <gahskp@gmail.com>
2019-03-12 08:38:14 -0300

    more migrations and conditionals

--- application/models/Vendas_model.php
+++ application/models/Vendas_model.php
@@ -3,5 +3,9 @@
 class Vendas_model extends CI_Model {
     public function salva($venda){
         $this->db->insert("vendas", $venda);
+        $this->db->update("produtos",
+            array("vendido" => TRUE),
+            array("id" => $venda["produto_id"])
+        );
     }
 }
